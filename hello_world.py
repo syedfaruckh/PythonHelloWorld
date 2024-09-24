525bb268c755294ddd47b6c1fd3ab06bc5844f4f
syedfaruckh <syedfaruckh@gmail.com>
2024-09-24 21:48:48 +0000

    update hello_world.py by asking user for name

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,13 @@
+""" 
+This is my first program. It outputs Hello World! to the screen
+S. Ali - September 2024
+"""
+
+def main(): 
+    name: str = input("what is your name? ")
+    print("Hello", name, "! its nice to meet you. ")
 
 
-
-def main():
-  pass
 
 
 
